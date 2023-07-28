b7a09d8a6b3f521fcf85aa3838013ad5ab3c3ccd
bhendi-boi <bhendiisgreen@gmail.com>
2023-07-28 10:13:02 +0530

    Without animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
+import netflix from "/netflix.png";
+import red from "/red.webp";
+import blue from "/blue.webp";
+import green from "/green.webp";
+
 function App() {
   return (
-    <main className="min-h-screen bg-fuchsia-400 grid place-items-center">
-      <div className="h-48 bg-white p-8 rounded-lg backdrop-blur-lg">
-        <h1 className="mb-8 font-sans text-2xl font-bold text-gray-900">
-          React Starter template with tailwind
+    <main className="min-h-screen bg-gray-950">
+      <header className="">
+        <h1>
+          <img src={netflix} alt="Netflix Logo" className="mx-auto" />
         </h1>
-        <p>
-          Visit the{" "}
-          <a
-            target="_blank"
-            rel="norefer"
-            href="https://github.com/bhendi-boi/vite-tailwind-starter-template"
-            className="cursor-pointer text-blue-600 font-medium"
-          >
-            git repo
-          </a>{" "}
-          .
-          <br />
-          If you found this useful dont forget to give it a ⭐ on github.
-        </p>
-      </div>
+      </header>
+      <section className="flex flex-col items-center justify-center gap-8 text-gray-50">
+        <h2 className="text-xl">Who's watching ?</h2>
+        <div className="grid grid-cols-2 gap-8 text-xs">
+          <div className="flex flex-col items-center gap-2 cursor-pointer">
+            <img
+              src={red}
+              alt="Person1"
+              className="w-16 rounded-sm aspect-square"
+            />
+            <p>Person 1</p>
+          </div>
+          <div className="flex flex-col items-center gap-2 cursor-pointer">
+            <img
+              src={blue}
+              alt="Person2"
+              className="w-16 rounded-sm aspect-square"
+            />
+            <p>Person 2</p>
+          </div>
+          <div className="flex flex-col items-center gap-2 cursor-pointer">
+            <img
+              src={green}
+              alt="Person3"
+              className="w-16 rounded-sm aspect-square"
+            />
+            <p>Person 3</p>
+          </div>
+          <div className="flex flex-col items-center gap-2 cursor-pointer">
+            <div className="flex items-center justify-center w-16 border rounded-sm aspect-square border-gray-50/50">
+              <div>
+                <div className="w-6 h-px bg-gray-50"></div>
+                <div className="w-6 h-px rotate-90 bg-gray-50"></div>
+              </div>
+            </div>
+            <p>Add Profile</p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
